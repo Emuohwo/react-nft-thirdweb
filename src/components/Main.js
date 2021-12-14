75c7c81f3fb52f22fe9995a6585aef1f1da0fa42
Emuohwo <warriisaac@outlook.com>
2021-12-13 20:59:55 -1100

    design punk list

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className='mainContent'>
+                <div className='punkHighlight'>
+                    <div className='punkContainer'>
+                        <img 
+                            className='selectedPunk'
+                          src={activePunk.image_original_url} 
+                          alt='' />
+                    </div>
+                </div>
+
+                <div className='punkDetails' style={{ color: "#fff"}}>
+                    <div className='title'>{active</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
